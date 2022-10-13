e97ebecc36153e516ec3505062c3418af9651ef0
AsifSyeed <asifsyeed666@gmail.com>
2022-10-13 17:23:54 +0600

    Count View added

--- Metronome/ViewController.swift
+++ Metronome/ViewController.swift
@@ -11,18 +11,24 @@ import AVFoundation
 class ViewController: UIViewController {
     @IBOutlet weak var tempoLabel: UILabel!
     @IBOutlet weak var playButtonImage: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
     
     var isPlay: Bool = false
     var tempoValue: Int = 60 {
         didSet {
             tempoLabel.text = String(tempoValue)
             if isPlay {
-                stopTimer()
+                timer?.invalidate()
+                timer = nil
                 startTimer()
             }
         }
     }
-    var count: Int = 0
+    var count: Int = 1 {
+        didSet {
+            countLabel.text = String(count)
+        }
+    }
     var timer: Timer?
     var audioPlayer: AVAudioPlayer?
     
@@ -34,6 +40,7 @@ class ViewController: UIViewController {
     func initialSetup() {
         isPlay = false
         tempoLabel.text = String(tempoValue)
+        countLabel.text = String(count)
     }
     
     func startTimer() {
@@ -46,31 +53,41 @@ class ViewController: UIViewController {
     func stopTimer() {
         timer?.invalidate()
         timer = nil
-        count = 0
+        count = 1
     }
     
     func playMetronomeAudio() {
-        guard let soundPath = Bundle.main.path(forResource: "stick", ofType: "mp3") else { return }
-        let url = URL(fileURLWithPath: soundPath)
+        let url: URL
+        
+        if count == 1 {
+            guard let soundPath = Bundle.main.path(forResource: "stick", ofType: "mp3") else { return }
+            url = URL(fileURLWithPath: soundPath)
+        } else {
+            guard let soundPath = Bundle.main.path(forResource: "stick_low", ofType: "mp3") else { return }
+            url = URL(fileURLWithPath: soundPath)
+        }
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: url)
             audioPlayer?.play()
+            
+            print(count)
             print("played")
         } catch {
             print(error)
         }
     }
     
-    @objc func fireTimer() {
+    func updateCount() {
         count += 1
-        playMetronomeAudio()
-        print(count)
-        if count % 2 == 0 {
-            tempoLabel.font = tempoLabel.font.withSize(150)
-        } else {
-            tempoLabel.font = tempoLabel.font.withSize(100)
+        if count > 4 {
+            count = 1
         }
+    }
+    
+    @objc func fireTimer() {
+        updateCount()
+        playMetronomeAudio()
     }
     
     @IBAction func playButtonAction(_ sender: Any) {
@@ -80,6 +97,7 @@ class ViewController: UIViewController {
             playButtonImage.image = UIImage(systemName: "play.circle.fill")
         } else {
             isPlay = true
+            playMetronomeAudio()
             startTimer()
             playButtonImage.image = UIImage(systemName: "pause.circle.fill")
         }
